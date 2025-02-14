08f167c4a95a99c9bc0e1d66efff0ccaa44417b9
Jade Nash <jade@lilydev.com>
2025-02-13 20:23:56 -0600

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+# Adapted from https://fasterthanli.me/series/building-a-rust-service-with-nix/part-10
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs =
+    {
+      self, # although nixd thinks otherwise, this is required
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        nativeBuildInputs = with pkgs; [
+          rustToolchain
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          openssl
+          # gcc
+        ];
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          inherit buildInputs nativeBuildInputs;
+        };
+      }
+    );
+}
