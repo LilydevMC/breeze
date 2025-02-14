32378cbbd94248b1d1f6cd744a59db9dd41ec441
Jade Nash <jade@lilydev.com>
2025-02-14 00:12:15 -0600

    Basic poise app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
+use poise::{serenity_prelude as serenity, Framework, FrameworkOptions, PrefixFrameworkOptions};
+use serenity::{ClientBuilder, GatewayIntents};
+
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+#[poise::command(slash_command, prefix_command)]
+async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.say("Pong!").await?;
+    Ok(())
+}
+
+#[dotenvy::load(required = false)]
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let discord_token = std::env::var("DISCORD_TOKEN").unwrap();
+
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::GUILD_MESSAGES;
+
+    let framework = Framework::builder()
+        .options(FrameworkOptions {
+            commands: vec![ping()],
+            prefix_options: PrefixFrameworkOptions {
+                prefix: Some("wl;".to_string()),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let client = ClientBuilder::new(discord_token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap();
 }
